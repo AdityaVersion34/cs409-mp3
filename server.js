c2106de5c873881216087b6110854ec07b4e0d66
AdityaVersion34 <112605343+AdityaVersion34@users.noreply.github.com>
2025-11-05 21:43:58 -0600

    reverting fix

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(bodyParser.json());
 require('./routes')(app, router);
 
 // Start the server
-app.listen(port, '0.0.0.0', () => {
-    console.log('Server running on port ' + port);
-});
+app.listen(port);
+// app.listen(port, '0.0.0.0', () => {
+//     console.log('Server running on port ' + port);
+// });
 console.log('Server running on port ' + port);
